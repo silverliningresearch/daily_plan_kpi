e9556158796d3e8632bcdae9b43798c10b94a90d
unknown <lb@silverliningresearch.com>
2024-02-01 16:07:44 +0000

    update new data

--- CEB_QSM/interview_statistics.js
+++ CEB_QSM/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-20",
         "quota_id": "110-BANGKOK SUVARNABHUMI",
         "Number of interviews": 4,
-        "download_time": "01-02-2024 16:52:48"
+        "download_time": "01-02-2024 17:07:40"
     },
     {
         "InterviewDate": "2024-01-20",
