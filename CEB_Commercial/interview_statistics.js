8ce6e5d77eed1a2c657e648f88c72fa1df88fc3a
unknown <lb@silverliningresearch.com>
2024-02-04 13:37:42 +0000

    update new data

--- CEB_Commercial/interview_statistics.js
+++ CEB_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-20",
         "quota_id": "110-BANGKOK SUVARNABHUMI",
         "Number of interviews": 4,
-        "download_time": "04-02-2024 14:22:45"
+        "download_time": "04-02-2024 14:37:41"
     },
     {
         "InterviewDate": "2024-01-20",
@@ -1268,7 +1268,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-02-04",
         "quota_id": "167-MOSCOW - SHEREMETYOVO",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-02-04",
@@ -1279,6 +1279,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "207-DUBAI",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-02-04",
+        "quota_id": "209-DUBAI",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-02-04",
