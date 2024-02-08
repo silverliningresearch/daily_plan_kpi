fda10468d91036723078300438a2cb628dc0ac3e
unknown <lb@silverliningresearch.com>
2024-02-08 16:07:41 +0000

    update new data

--- CEB_QSM/interview_statistics.js
+++ CEB_QSM/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "5J-BAG",
         "Number of interviews": 1,
-        "download_time": "08-02-2024 16:52:37"
+        "download_time": "08-02-2024 17:07:38"
     },
     {
         "InterviewDate": "2024-02-05",
@@ -24,6 +24,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-08",
         "quota_id": "7C-TAE",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-02-08",
+        "quota_id": "7C-UNKNOWN",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-02-08",
