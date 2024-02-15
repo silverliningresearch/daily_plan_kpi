d90d5aee2c9f44ec490bd0f3f8a1ef00c199c260
unknown <lb@silverliningresearch.com>
2024-02-15 18:07:40 +0000

    update new data

--- CEB_Commercial/interview_statistics.js
+++ CEB_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "TR-BCD",
         "Number of interviews": 1,
-        "download_time": "15-02-2024 18:52:40"
+        "download_time": "15-02-2024 19:07:40"
     },
     {
         "InterviewDate": "2024-02-07",
@@ -403,6 +403,11 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-02-15",
         "quota_id": "SQ-SIN",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-02-15",
+        "quota_id": "TK-IST",
         "Number of interviews": 1
     },
     {
--- CEB_QSM/interview_statistics.js
+++ CEB_QSM/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "5J-BAG",
         "Number of interviews": 1,
-        "download_time": "15-02-2024 18:52:37"
+        "download_time": "15-02-2024 19:07:38"
     },
     {
         "InterviewDate": "2024-02-05",
@@ -329,6 +329,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-15",
         "quota_id": "5J-HKG",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-02-15",
+        "quota_id": "7C-PUS",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-02-15",
