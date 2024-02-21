7dd0418cafcf3191778e949d7ffd2c9c1e0db7b0
unknown <lb@silverliningresearch.com>
2024-02-21 22:22:41 +0000

    update new data

--- CEB_Commercial/interview_statistics.js
+++ CEB_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "TR-BCD",
         "Number of interviews": 1,
-        "download_time": "21-02-2024 23:07:41"
+        "download_time": "21-02-2024 23:22:41"
     },
     {
         "InterviewDate": "2024-02-07",
@@ -947,13 +947,18 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-02-21",
+        "quota_id": "2P-USU",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-02-21",
         "quota_id": "5J-CRK",
         "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-02-21",
         "quota_id": "5J-MNL",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-02-21",
@@ -1024,6 +1029,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-21",
         "quota_id": "PR-MNL",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-02-21",
+        "quota_id": "PR-USU",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-02-21",
--- CEB_QSM/interview_statistics.js
+++ CEB_QSM/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "5J-BAG",
         "Number of interviews": 1,
-        "download_time": "21-02-2024 23:07:39"
+        "download_time": "21-02-2024 23:22:38"
     },
     {
         "InterviewDate": "2024-02-05",
@@ -894,6 +894,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-21",
         "quota_id": "2P-MPH",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-02-21",
+        "quota_id": "2P-USU",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-02-21",
