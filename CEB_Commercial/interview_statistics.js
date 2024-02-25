562b6518fb8d41d0dac97f82d6c881d11ec2e8c9
unknown <lb@silverliningresearch.com>
2024-02-25 09:07:43 +0000

    update new data

--- CEB_Commercial/interview_statistics.js
+++ CEB_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "TR-BCD",
         "Number of interviews": 1,
-        "download_time": "25-02-2024 09:52:45"
+        "download_time": "25-02-2024 10:07:43"
     },
     {
         "InterviewDate": "2024-02-07",
@@ -1517,6 +1517,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-02-25",
+        "quota_id": "5J-HKG",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-02-25",
         "quota_id": "5J-ICN",
         "Number of interviews": 2
     },
@@ -1533,7 +1538,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-02-25",
         "quota_id": "DG-BXU",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-02-25",
--- CEB_QSM/interview_statistics.js
+++ CEB_QSM/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "5J-BAG",
         "Number of interviews": 1,
-        "download_time": "25-02-2024 09:52:40"
+        "download_time": "25-02-2024 10:07:39"
     },
     {
         "InterviewDate": "2024-02-05",
@@ -1467,8 +1467,18 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-02-25",
+        "quota_id": "5J-CGY",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-02-25",
         "quota_id": "5J-CRK",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-02-25",
+        "quota_id": "5J-HKG",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-02-25",
@@ -1478,7 +1488,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-02-25",
         "quota_id": "CX-HKG",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-02-25",
