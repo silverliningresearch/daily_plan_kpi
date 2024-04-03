742ab23fdcbd442d4df65062972cc19fb00bcafa
unknown <lb@silverliningresearch.com>
2024-04-03 20:07:57 +0000

    update new data

--- CEB_QSM/interview_statistics.js
+++ CEB_QSM/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "te-BAG",
         "Number of interviews": 1,
-        "download_time": "04-04-2024 03:52:44"
+        "download_time": "04-04-2024 04:07:45"
     },
     {
         "InterviewDate": "2024-02-05",
