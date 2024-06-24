504dffb3e9dde49eac5637fd15e26b7bf25caf17
unknown <lb@silverliningresearch.com>
2024-06-24 11:07:58 +0000

    update new data

--- CEB_Commercial/interview_statistics.js
+++ CEB_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "TE-BCD",
         "Number of interviews": 1,
-        "download_time": "24-06-2024 18:53:01"
+        "download_time": "24-06-2024 19:07:58"
     },
     {
         "InterviewDate": "2024-02-07",
